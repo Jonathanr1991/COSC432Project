234fbcb35a319bd596ea6cef91d99dce92049d30
jrodriquez <jrodri30@students.towson.edu>
2019-10-30 14:44:18 -0400

    added script to seperate data into 3 different files

---
+++ cleaning_data_file.py
@@ -0,0 +1,44 @@
+
+filepath = "Consolidated_data.txt"
+
+text = open(filepath, "r")
+#creating a file where to place the training data 40%
+training = open("training.txt", "w")
+#creating a file where to place the validating data 20%
+validate = open("validate.txt", "w")
+#creating a file where to place the testing data 20%
+testing = open("testing.txt", "w")
+
+performance =[]
+maintainability=[]
+operational=[]
+security=[]
+usability=[]
+
+
+#reads in lines and picks out nonfunctional requirements with the corresponding categories  
+for x in text: 
+
+    if ("performance" in x):
+        performance.append(x)
+    if ("maintainability" in x):
+        maintainability.append(x)
+    if ("operational" in x):
+        operational.append(x)
+    if ("security" in x):
+        security.append(x)
+    if ("usability" in x):
+        usability.append(x)
+
+count = 1
+
+for y in performance:
+    if (count==1 or count==2):
+        training.write(y)
+        count+1
+    if (count==3):
+        validate.write(y)
+        count+1
+    if (count==4):
+        testing.write(y)
+        count=1
